11a69c7483662044a5fe78f0ffe6b8695b29c135
agajdosi <agajdosi@redhat.com>
2018-08-14 15:40:17 +0200

    Issue #154 Remove Che tests

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -7,8 +7,6 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/minishift/minishift-addons/test/integration/che"
-
 	"github.com/DATA-DOG/godog"
 	"github.com/minishift/minishift/test/integration/testsuite"
 )
@@ -37,7 +35,7 @@ func TestMain(m *testing.M) {
 func getFeatureContext(s *godog.Suite) {
 	// loads step definitions from the Minishift integration testsuite
 	testsuite.FeatureContext(s)
-	che.FeatureContext(s)
+
 	// loads additional step definitions for minishift-addons
 	// mypackage.FeatureContext(s)
 }
